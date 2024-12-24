c7f97f9ec40100a74d73706710f11e32ea35565e
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2024-12-24 21:05:08 +0100

    Updated index.js in src

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ListStudents from "./cc1/part1/ListeStudents";
+import ListePersonnalisable from "./cc1/part2/ListePersonnalisable";
+import Login from "./tp/Login";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+      <div>
+          <Login />
+      </div>
 );
 
 // If you want to start measuring performance in your app, pass a function
