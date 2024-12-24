f91d5ba594e5b206060764230edc5e2933a09c55
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2024-12-24 21:05:10 +0100

    Updated HeaderTable.jsx in src/cc1/part1

---
+++ src/cc1/part1/HeaderTable.jsx
@@ -0,0 +1,15 @@
+export default function HeaderTable() {
+    // render the header table columns .
+    return(
+        <thead>
+            <tr>
+                <th>first name</th>
+                <th>last name</th>
+                <th>birth date</th>
+                <th>gender</th>
+                <th>city</th>
+                <th>average</th>
+            </tr>
+        </thead>
+    )
+}
