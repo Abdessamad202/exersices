0be65befcba1f3dc1ffcbc825115c71072bc2161
Abdessamad Kechaf <kechaf.abdo.ka.2018@gmail.com>
2024-12-25 21:09:23 +0100

    Updated data.jsx in src/cc1/part3

---
+++ src/cc1/part3/data.jsx
@@ -0,0 +1,22 @@
+export const data = [
+  {
+    'name' : "Laptop",
+    'category' : "Electronic",
+    'price' : 200
+  },
+  {
+    'name' : "Chair",
+    'category' : "Furniture",
+    'price' : 150
+  },
+  {
+    'name' : "SmartPhone",
+    'category' : "Electronic",
+    'price' : 800
+  },
+  {
+    'name' : "Table",
+    'category' : "Furniture",
+    'price' : 300
+  }
+]
